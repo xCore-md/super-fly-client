ca7608343bbc49a7719f3caaaecc61b56f106287
cralex11 <45730710+cralex11@users.noreply.github.com>
2024-05-02 18:45:15 +0300

    disabled dark theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,10 @@
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <div>
+      Header Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+      error excepturi expedita inventore ipsum natus nihil omnis repellat
+      soluta? Ab assumenda, dolor esse magni odio perspiciatis reiciendis
+      reprehenderit vitae voluptatum.
+    </div>
+  )
 }
