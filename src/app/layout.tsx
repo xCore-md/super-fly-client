c9894377ba3d3109b98e7de9a5489f20cb2ac848
cralex11 <45730710+cralex11@users.noreply.github.com>
2024-05-03 15:38:05 +0300

    installed shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter, Poppins } from 'next/font/google'
+import '../styles/globals.css'
 import { Header } from '@/components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Poppins({ subsets: ['latin'], weight: ['400', '700'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
+        <main className="container mt-6 mx-auto">{children}</main>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
 export default function Home() {
-  return <main className="container">lorem ipsum</main>
+  return (
+    <section className="bg-blue-700 rounded-bl-xl rounded-br-xl text-white">
+      <header>
+        <h1>Incepe Calatoria Ta</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+          doloribus fugiat hic id incidunt ipsa itaque libero nam nemo nostrum
+          numquam officia placeat quo recusandae, reiciendis vel veniam
+          veritatis. Quo.
+        </p>
+      </header>
+      <main>
+        <Tabs>
+          <TabsList>
+            <TabsTrigger value="dus">Dus</TabsTrigger>
+            <TabsTrigger value="intors">Intors</TabsTrigger>
+          </TabsList>
+          <TabsContent value={'dus'}>dus content</TabsContent>
+          <TabsContent value={'intors'}>intors content</TabsContent>
+        </Tabs>
+      </main>
+    </section>
+  )
 }
