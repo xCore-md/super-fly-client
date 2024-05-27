c0f80d02ee1ea9577f818ebb15feb63a938ea6f0
cralex11 <45730710+cralex11@users.noreply.github.com>
2024-05-27 17:02:37 +0300

    sticky ReservationSummary

--- src/components/reservation/reservation-summary.tsx
+++ src/components/reservation/reservation-summary.tsx
@@ -26,86 +26,88 @@ export const ReservationSummary = () => {
 
   return (
     <section className="flex flex-1 flex-col">
-      <div className="hidden flex-col gap-7 lg:flex">
-        <SectionLightBlue>
+      <div className="lg:sticky lg:top-32">
+        <div className="hidden flex-col gap-7 lg:flex">
+          <SectionLightBlue>
+            <div>
+              <h6 className="text-xs font-bold text-[#121C5E]">
+                Chisinau - Milano
+              </h6>
+              <p className="mt-1 text-xxs text-[#9D9D9D]">
+                Dus - Întors - 1 Adult
+              </p>
+            </div>
+            <Button
+              className="rounded-full border-brand-blue bg-transparent text-sm text-brand-blue"
+              variant="outline"
+            >
+              Editează
+            </Button>
+          </SectionLightBlue>
+
           <div>
-            <h6 className="text-xs font-bold text-[#121C5E]">
-              Chisinau - Milano
-            </h6>
-            <p className="mt-1 text-xxs text-[#9D9D9D]">
-              Dus - Întors - 1 Adult
-            </p>
+            <SectionLightBlue className="text-sm font-bold text-[#121C5E]">
+              <h6>Nume și Prenume</h6>
+            </SectionLightBlue>
+            <div className="mx-4 mt-2 flex gap-5">
+              <Input
+                className="h-9 px-3 text-xs"
+                type="text"
+                placeholder="Prenume"
+              />
+              <Input
+                className="h-9 px-3 text-xs"
+                type="text"
+                placeholder="Nume"
+              />
+            </div>
           </div>
-          <Button
-            className="rounded-full border-brand-blue bg-transparent text-sm text-brand-blue"
-            variant="outline"
-          >
-            Editează
-          </Button>
-        </SectionLightBlue>
 
-        <div>
           <SectionLightBlue className="text-sm font-bold text-[#121C5E]">
-            <h6>Nume și Prenume</h6>
+            <h6>Bagaje</h6>
           </SectionLightBlue>
-          <div className="mx-4 mt-2 flex gap-5">
-            <Input
-              className="h-9 px-3 text-xs"
-              type="text"
-              placeholder="Prenume"
-            />
-            <Input
-              className="h-9 px-3 text-xs"
-              type="text"
-              placeholder="Nume"
-            />
+
+          <div>
+            <SectionLightBlue className="flex justify-between text-sm font-bold text-[#121C5E]">
+              <h6>Servicii</h6>
+              <h6>€30</h6>
+            </SectionLightBlue>
+
+            {flightData.map((flight) => (
+              <FlightInfo key={flight.route} {...flight} />
+            ))}
           </div>
         </div>
 
-        <SectionLightBlue className="text-sm font-bold text-[#121C5E]">
-          <h6>Bagaje</h6>
-        </SectionLightBlue>
-
-        <div>
-          <SectionLightBlue className="flex justify-between text-sm font-bold text-[#121C5E]">
-            <h6>Servicii</h6>
-            <h6>€30</h6>
-          </SectionLightBlue>
-
-          {flightData.map((flight) => (
-            <FlightInfo key={flight.route} {...flight} />
-          ))}
+        <div className="mt-4 rounded-full bg-brand-blue px-4 py-3 text-xs text-white lg:mt-11">
+          Total: <span className="font-bold">€466</span>
         </div>
+
+        <p className="mt-9 text-center text-lg font-bold lg:text-left">
+          Prețul expiră în:
+          <span className="ml-1 text-red-600">12m : 30s</span>
+        </p>
+
+        <Progress
+          value={23}
+          progressClassName="bg-red-600 "
+          className="mt-4 h-0.5 w-[100%] bg-[#E7E7E7]"
+          showRectangle
+        />
+
+        <p className="mt-3 text-xxs text-[#9D9D9D]">
+          <span className="font-bold">Economisești timp și bani:</span> Exemplu
+          text, in care va fi indicat anuntul despre disponibilitatea pretului,
+          si propunerea de a rezerva acum, pentru a nu pierde oferta la pret.
+        </p>
+
+        <Link
+          href="/confirm-reservation"
+          className="mt-8 flex h-11 items-center justify-center rounded-full bg-brand-green px-8 font-light text-white shadow-md shadow-slate-400 lg:hidden"
+        >
+          Rezervă acum
+        </Link>
       </div>
-
-      <div className="mt-4 rounded-full bg-brand-blue px-4 py-3 text-xs text-white lg:mt-11">
-        Total: <span className="font-bold">€466</span>
-      </div>
-
-      <p className="mt-9 text-center text-lg font-bold lg:text-left">
-        Prețul expiră în:
-        <span className="ml-1 text-red-600">12m : 30s</span>
-      </p>
-
-      <Progress
-        value={23}
-        progressClassName="bg-red-600 "
-        className="mt-4 h-0.5 w-[100%] bg-[#E7E7E7]"
-        showRectangle
-      />
-
-      <p className="mt-3 text-xxs text-[#9D9D9D]">
-        <span className="font-bold">Economisești timp și bani:</span> Exemplu
-        text, in care va fi indicat anuntul despre disponibilitatea pretului, si
-        propunerea de a rezerva acum, pentru a nu pierde oferta la pret.
-      </p>
-
-      <Link
-        href="/confirm-reservation"
-        className="mt-8 flex h-11 items-center justify-center rounded-full bg-brand-green px-8 font-light text-white shadow-md shadow-slate-400 lg:hidden"
-      >
-        Rezervă acum
-      </Link>
     </section>
   )
 }
