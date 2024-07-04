aa0a7dc0935cf548ac7e9c1c304710490d9a0279
Aeronhide <c.a.emberd@gmail.com>
2024-07-04 12:33:23 +0300

    Update axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -9,7 +9,7 @@ axs.interceptors.request.use(
   (config) => {
     const storage = localStorage.getItem('userData')
     const userData = storage ? JSON.parse(storage) : null
-    const token = userData.token
+    const token = userData?.token
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`
     }
