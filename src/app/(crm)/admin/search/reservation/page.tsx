20b42d0dd69d5de906c0307be5d4e9efde926973
Aeronhide <c.a.emberd@gmail.com>
2024-08-09 01:13:11 +0300

    Fix props

--- src/app/(crm)/admin/search/reservation/page.tsx
+++ src/app/(crm)/admin/search/reservation/page.tsx
@@ -5,7 +5,7 @@ import { ReservationMainForm } from '@components/reservation/reservation-main-fo
 export default function Results() {
   return (
     <div>
-      <ReservationMainForm showBaggage={false} />
+      <ReservationMainForm passengersCount={0} showBaggage={false} />
       <Link
         href="/confirm-reservation"
         className="mx-auto mt-8 flex h-11 w-48 items-center justify-center rounded-full bg-brand-green px-8 font-light text-white shadow-md shadow-slate-400"
