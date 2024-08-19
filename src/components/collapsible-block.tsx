7bb16c4b85645c0a7eceb6012b1fa82140f6d76c
cralex11 <cristeaalex7@gmail.com>
2024-08-19 18:25:29 +0300

    main: fixed the accordions

--- src/components/collapsible-block.tsx
+++ src/components/collapsible-block.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Image from 'next/image'
-import { useState } from 'react'
 import flyOne from '@/assets/img/fly-one.png'
 import minus from '@/assets/img/minus.svg'
 import plus from '@/assets/img/plus.svg'
@@ -13,31 +12,39 @@ import {
 import { FlyContent } from './flights/fly-content'
 import { Button } from './ui/button'
 
-const CollapsibleBlock = () => {
-  const [isOpen, setIsOpen] = useState(false)
-
+const CollapsibleBlock = ({
+  offer,
+  isOpen,
+  setIsOpen,
+}: {
+  offer: { title: string }
+  // eslint-disable-next-line no-unused-vars
+  isOpen: (title: string) => boolean
+  // eslint-disable-next-line no-unused-vars
+  setIsOpen: (title: string) => void
+}) => {
   return (
     <div className="flex items-start gap-2">
       <div className="w-full">
         <Collapsible
-          open={isOpen}
-          onOpenChange={setIsOpen}
+          open={isOpen(offer.title)}
+          onOpenChange={() => setIsOpen(offer.title)}
           className="rounded-full bg-white"
         >
           <CollapsibleTrigger asChild className="w-full">
             <div
-              className={`relative z-10 flex h-[63px] items-center justify-between rounded-full px-4 ${isOpen ? 'bg-blue-700' : 'border border-gray-200 bg-white'}`}
+              className={`relative z-10 flex h-[63px] items-center justify-between rounded-full px-4 ${isOpen(offer.title) ? 'bg-blue-700' : 'border border-gray-200 bg-white'}`}
             >
               <span
-                className={`text-lg font-light ${isOpen ? 'text-white' : 'text-black'}`}
+                className={`text-lg font-light ${isOpen(offer.title) ? 'text-white' : 'text-black'}`}
               >
-                Chișinău - Milano
+                Chișinău - {offer.title}
               </span>
               <Button
-                className={`h-[38px]  w-[38px] rounded-full p-0 ${isOpen ? 'bg-white' : 'bg-blue-700 shadow-md shadow-slate-400'}`}
+                className={`h-[38px]  w-[38px] rounded-full p-0 ${isOpen(offer.title) ? 'bg-white' : 'bg-blue-700 shadow-md shadow-slate-400'}`}
               >
                 <Image
-                  src={isOpen ? minus : plus}
+                  src={isOpen(offer.title) ? minus : plus}
                   alt="plus-icon"
                   width={18}
                   height={38}
@@ -46,7 +53,7 @@ const CollapsibleBlock = () => {
             </div>
           </CollapsibleTrigger>
           <CollapsibleContent
-            className={`-mt-10 rounded-b-[20px] bg-white px-5 pb-4 pt-14 ${isOpen ? 'shadow-lg shadow-slate-200' : ''}`}
+            className={`-mt-10 rounded-b-[20px] bg-white px-5 pb-4 pt-14 ${isOpen(offer.title) ? 'shadow-lg shadow-slate-200' : ''}`}
           >
             {/*desktop todo: check if the variation on mobile and desktop is right*/}
             <div className="hidden grid-cols-4 lg:grid">
@@ -88,19 +95,19 @@ const CollapsibleBlock = () => {
       </div>
       <div className="hidden lg:w-1/5">
         <Collapsible
-          open={isOpen}
-          onOpenChange={setIsOpen}
+          open={isOpen(offer.title)}
+          onOpenChange={() => setIsOpen(offer.title)}
           className="rounded-full bg-white"
         >
           <CollapsibleTrigger className="w-full">
             <div
-              className={`flex h-[63px] items-center justify-center rounded-full px-4 ${isOpen ? 'bg-blue-700 text-white' : 'border border-gray-200 bg-white'}`}
+              className={`flex h-[63px] items-center justify-center rounded-full px-4 ${isOpen(offer.title) ? 'bg-blue-700 text-white' : 'border border-gray-200 bg-white'}`}
             >
               <span className="text-xl">MDL 2299</span>
             </div>
           </CollapsibleTrigger>
           <CollapsibleContent
-            className={`-mt-10 rounded-b-[20px] bg-white px-6 pb-1 pt-16 ${isOpen ? 'shadow-lg shadow-slate-200' : ''}`}
+            className={`-mt-10 rounded-b-[20px] bg-white px-6 pb-1 pt-16 ${isOpen(offer.title) ? 'shadow-lg shadow-slate-200' : ''}`}
           >
             <Button className="mb-6 h-[46px] w-full rounded-full bg-blue-700 px-4 text-lg font-light shadow-md shadow-slate-400">
               <span>Alege</span>
