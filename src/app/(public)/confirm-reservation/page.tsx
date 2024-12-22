579f157c751390666948ffe8500fa6d283a69c8f
Aeronhide <c.a.emberd@gmail.com>
2024-12-22 15:02:56 +0200

    Remove price round

--- src/app/(public)/confirm-reservation/page.tsx
+++ src/app/(public)/confirm-reservation/page.tsx
@@ -538,7 +538,7 @@ export default function ConfirmReservationPage() {
             <div>
               <span className="font-light">Total:</span>
               <span className="ml-2 font-semibold">
-                {Math.round(res.price + baggagePrice + servicePrice)} €
+                {res.price + baggagePrice + servicePrice} €
               </span>
             </div>
           </div>
@@ -703,7 +703,7 @@ export default function ConfirmReservationPage() {
             <div>
               <span className="font-light">{t.total}:</span>
               <span className="ml-2 font-semibold">
-                {Math.round(res.price + baggagePrice + servicePrice)} €
+                {res.price + baggagePrice + servicePrice} €
               </span>
             </div>
           </div>
